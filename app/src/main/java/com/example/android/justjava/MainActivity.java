7d3d3d870a543f039e7325bdd60fcfc94722127d
Alex Panov <alexeypd@gmail.com>
2019-11-05 16:00:34 -0500

    implemented logic

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -15,27 +15,48 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.NumberFormat;
+
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
 
+    private int numberOfOrderedCoffees;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("quantity", numberOfOrderedCoffees);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        numberOfOrderedCoffees = savedInstanceState.getInt("quantity");
+        display(numberOfOrderedCoffees);
+    }
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        displayPrice(numberOfOrderedCoffees);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
+
+    public void displayPrice(int number){
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
@@ -43,13 +64,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void plus(View view) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        int x = Integer.parseInt((String) quantityTextView.getText()) + 1;
-        display(x);
+        numberOfOrderedCoffees = Integer.parseInt((String) quantityTextView.getText()) + 1;
+        display(numberOfOrderedCoffees);
     }
 
     public void minus(View view) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        int x = Integer.parseInt((String) quantityTextView.getText()) - 1;
-        display(x);
+        numberOfOrderedCoffees = Integer.parseInt((String) quantityTextView.getText()) - 1;
+        display(numberOfOrderedCoffees);
     }
 }
